5d4e4956ac5ab416f84e16b565c8175052de0b88
akpalinov <akpalinov>
2024-12-15 17:22:06 +0500

    Initial Commit

---
+++ Lesson 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.0
+//
+//  Created by Bakhtiyar Akpalinov on 15.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
